33b9b28533c6fb086552a46312ce3ff7ead5bc68
Philip Ajuong Deng <philipajuong@gmail.com>
2024-07-17 17:23:54 +0200

    printf.c

--- _printf.c
+++ _printf.c
@@ -1,27 +1,43 @@
 #include "main.h"
-
 /**
- * _printf - display output on a format based
- * @format: the string character
- * Return: return the string else NULL
- **/
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
+ */
 int _printf(const char *format, ...)
 {
-	int size;
-	va_list args;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (format == NULL)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
-
-	va_start(args, format);
-	size = handler(format, args);
-
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
 	_putchar(-1);
-	va_end(args);
-
-	return (size);
+	va_end(arguments);
+	return (count);
 }
